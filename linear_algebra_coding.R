cb41d411447a41ab0bcd96eb8e194f53e0d3ea96
Mena Whalen <mwhalen3@luc.edu>
2024-01-30 12:39:06 -0600

    adding new code for the week

--- linear_algebra_coding.R
+++ linear_algebra_coding.R
@@ -80,3 +80,66 @@ acos(x%*%y/(sqrt(sum(x^2))*sqrt(sum(y^2))))
 ## this is in radians
 ## need to multiply by 180/pi to be degrees
 acos(x%*%y/(sqrt(sum(x^2))*sqrt(sum(y^2))))*180/pi
+
+
+
+############### notes for week of 1/30-2/1
+?matrix
+as.matrix(receipt)
+
+##best cheatsheet
+# https://www.statmethods.net/advstats/matrix.html
+
+a <- matrix(c(3,1,-1,5,2,4), nrow = 2)
+b <- matrix(c(-2,7,9), ncol = 1)
+## wont work here
+a*b
+a%*%b
+
+## transpose
+?t()
+t(a)
+
+
+## square matrix finding inverse
+A <- matrix(c(1,-5,-5,1), ncol = 2)
+
+solve(A)
+
+A %*% solve(A) ## basically identity matrix
+round(A %*% solve(A), digits = 1)
+
+## another way to get the identity matrix
+diag(2) ## creates I by number put in
+
+diag(A) ## gives you the diagnol of a matrix
+
+#########################
+## eigen value/vector
+
+x1 <- c(1/sqrt(2), -1/sqrt(2))
+A %*% x1
+6 %*% x1
+## x1 eigen vector and 6 eigen value
+x2 <- c(1/sqrt(2), 1/sqrt(2))
+A %*% x2
+-4 %*% x2
+## x2 eigen vector and -4 eigen value
+
+## easier
+eigen(A)
+
+
+## spectral decomp
+A <- matrix(c(13,-4,2,-4,13,-2,2,-2,10), ncol = 3)
+E <- eigen(A)
+L <- E$values
+V <- E$vectors
+A1 <- L[1] * V[,1] %*% t(V[,1])
+A2 <- L[2] * V[,2] %*% t(V[,2])
+A3 <- L[3] * V[,3] %*% t(V[,3])
+A1 + A2 +A3
+## can be used also but ehhhh
+svd(A)
+
+
